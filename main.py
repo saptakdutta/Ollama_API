fccd74922c9322f28e3464b6a483f2da44ec6611
saptakdutta <saptrak.dutta@gmail.com>
2024-07-19 13:41:46 -0400

    docker command updates

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import json
 
 # %% Set API parameters
 # to set up docker ollama use: docker run -d --gpus=all -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama
+# to start the ollama container: docker start ollama
 # to download a new model use ex: docker exec -it ollama ollama run llama2:
 # to check is ollama is running visit: http://localhost:11434/ in your browser
 
@@ -15,7 +16,8 @@ url = "http://localhost:11434/api/generate"
 headers = {"Content-Type": "applicationm/json"}
 data = {
     "model": "mixtral:8x7b-instruct-v0.1-q3_K_M",
-    "prompt": "Write a letter to an contractong company explaining that my air conditioning unit is not functional and asking for a quote to get a new unit or repair the existing unit",
+    "prompt": "Write a letter to an contractong company explaining that my air conditioning\
+        unit is not functional and asking for a quote to get a new unit or repair the existing unit",
     "stream": False,
 }
 
